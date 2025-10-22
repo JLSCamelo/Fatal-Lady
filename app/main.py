9053ef618fa4fa9ed9d77da6e21628ed439a6feb
Mauricio Bertuci <bertuci.mauricio@gmail.com>
2025-10-22 14:35:10 -0300

    resolvendo merge

--- app/main.py
+++ app/main.py
@@ -1,30 +1,16 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from routes.produto_route import router as produto_router
 from routes.login_route import router as login_router
 from routes.cadastro_route import router as cadastro_router
-=======
-"""
-Nome: Mauricio Bertuci Saletti
-DATA: 08/10/2025
-RA:24722027
-"""
-
-from fastapi import FastAPI
-from controllers.controller_produtos import *
-from routes.produto_route import *
->>>>>>> 45833ccc4ef94b15890ebafc0aa288548945ec6e
 from fastapi.staticfiles import StaticFiles
 
-#montar pasta de imagem
+
 app = FastAPI(title="Loja de Sapatos")
 
 app.mount("/static", StaticFiles(directory="views/static"), name="static")
 
-<<<<<<< HEAD
 app.include_router(produto_router)    
 app.include_router(login_router)    
 app.include_router(cadastro_router)    
-=======
-app.include_router(router)    
->>>>>>> 45833ccc4ef94b15890ebafc0aa288548945ec6e
+
+ 
