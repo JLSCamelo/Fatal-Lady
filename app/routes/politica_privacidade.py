a6903d18c2394931c7c83e8bad4214457b83dce3
Mauricio Bertuci <bertuci.mauricio@gmail.com>
2025-11-17 23:56:05 -0300

    ajuste em rota pryvacy

--- app/routes/politica_privacidade.py
+++ app/routes/politica_privacidade.py
@@ -3,13 +3,16 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.database import get_db
+from app.models.usuario_model import UsuarioDB
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/views/templates")
 
 
 @router.get("/privacy", response_class=HTMLResponse)
-def home(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("politicadeprivacidade.html", {
-        "request": request 
-    })
+def privacy(request: Request, db: Session = Depends(get_db)):
+    usuario = None
+    return templates.TemplateResponse(
+        "politicadeprivacidade.html",
+        {"request": request, "usuario": usuario}
+    )
